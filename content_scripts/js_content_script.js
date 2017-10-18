6c9a2cc9352a1b28aca48ee7313d11ed2afcdd4b
Neobrinoke <alexis.salvador.pro@gmail.com>
2017-10-18 14:12:12 +0200

    AS - Add comments

--- content_scripts/js_content_script.js
+++ content_scripts/js_content_script.js
@@ -1,28 +1,30 @@
-var nCount = 0;
+var nCount = 0; // Création du compteur pour le nombre d'erreur qui sera peut-être afficher
 function js_content_script( request, sender, sendResponse )
 {
-	var messageResponse = "";
-	messageResponse += checkLangAttributeHtml();
+	var messageResponse = ""; // Création d'une variable (string) content tous mes messages d'erreurs
+	messageResponse += checkLangAttributeHtml(); // Ajout d'un message d'erreur via l'appel d'une fonction
 
-	if( nCount > 0 )
+	if( nCount > 0 ) // si le compteur d'erreur est supérieur à 0 ( au moins égale a 1 )
 	{
+		// On remonte le message 'il y a ..' au dessus de la liste des erreurs
 		messageResponse = "Il y a " + nCount + " problème(s) sur ce site\n\n" + messageResponse;
 
-		window.alert( messageResponse );
+		window.alert( messageResponse ); // Fait apparaitre un pop-up avec un message
 	}
-	else window.alert( "Aucun problème sur ce site" );
+	else window.alert( "Aucun problème sur ce site" ); // Fait apparaitre un pop-up avec un message
 
-	nCount = 0;
 	browser.runtime.onMessage.removeListener( js_content_script );
 }
 
-function checkLangAttributeHtml()
+function checkLangAttributeHtml() // Fonction qui regarde si l'élément html possède un attribus lang
 {
-	if( document.documentElement.hasAttribute("lang") === false ) {
-		nCount++;
-		return "Le selecteur <html> n'as pas d'attribus lang (<html lang=\"fr\">)\n";
+	// hasAttribute: retourne une value boolean
+	if( document.documentElement.hasAttribute("lang") === false ) // si l'élément html possède pas d'attribus lang
+	{
+		nCount++; // ajoute + 1 a la variable nCount
+		return "Le selecteur <html> n'as pas d'attribus lang (<html lang=\"fr\">)\n"; // Arrête l'éxecution de la fonction et retourne un message
 	}
-	return "";
+	return ""; // Arrête la fonction ici et retourne un message
 }
 
 browser.runtime.onMessage.addListener( js_content_script );
